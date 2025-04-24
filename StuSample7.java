203b1f831cd7aa9ec71315e333332eb2c8e0c138
shotatuchiya <121003827+ShotaTsuchiya@users.noreply.github.com>
2025-04-24 23:46:04 +0900

    4/24

---
+++ StuSample7.java
@@ -0,0 +1,11 @@
+public class StuSample7 {
+  public static void main(String[] args){
+    Student7 stu = new Student7();
+
+    stu.setName("菅原");
+    stu.display();
+
+    stu.setStuNo(1);
+    stu.displayStuNo();
+  }
+}
