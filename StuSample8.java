7b666a8677a5973634f04ed6f69bd37c8528cd4c
shotatuchiya <121003827+ShotaTsuchiya@users.noreply.github.com>
2025-04-27 23:14:37 +0900

    4/27

---
+++ StuSample8.java
@@ -0,0 +1,9 @@
+public class StuSample8 {
+  public static void main(String[] args){
+    final int BASE_NO = 1000;
+    Student8 stu1 = new Student8("菅原" ,BASE_NO+1);
+    stu1.display();
+    Student8 stu2 = new Student8("村山", BASE_NO+2);
+    stu2.display();
+  }
+}
---
+++ StuSample9.java
@@ -0,0 +1,6 @@
+public class StuSample9 {
+  public static void main(String[] args){
+    Student9 stu = new Student9("菅原");
+    stu.display();
+  }
+}
