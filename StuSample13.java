4fbfa960945cde30ff297dbd3da4f1237eff8f9b
shotatuchiya <121003827+ShotaTsuchiya@users.noreply.github.com>
2025-04-30 23:57:33 +0900

    4/30

---
+++ StuSample13.java
@@ -0,0 +1,8 @@
+import pack.Student13;
+
+public class StuSample13 {
+  public static void main(String[] args){
+    Student13 stu = new Student13("菅原");
+    stu.display();
+  }
+}
---
+++ Student13.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Student13 {
+  private String name;
+
+  public Student13(String name){
+    this.name = name;
+  }
+  public void display(){
+    System.out.println("名前：" + name);
+  }
+}
